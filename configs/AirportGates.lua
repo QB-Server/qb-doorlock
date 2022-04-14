348828e7374dec82185264662d14e3ad7e1c84d0
Bobbins <bobbins@protonmail.com>
2022-04-14 19:44:04 +0100

    Added airport gates

---
+++ configs/AirportGates.lua
@@ -0,0 +1,16 @@
+
+
+-- 1 created by Callum
+Config.DoorList['AirportGates-1'] = {
+    hideLabel = false,
+    objName = 725274945,
+    fixText = false,
+    authorizedJobs = { ['pilot'] = 0 },
+    objCoords = vec3(-990.296326, -2829.887207, 12.949859),
+    objYaw = 149.99998474121,
+    distance = 6,
+    doorRate = 2.0,
+    locked = true,
+    doorLabel = 'Airport Gates',
+    doorType = 'sliding',
+}
---
+++ configs/AirportGates2.lua
@@ -0,0 +1,15 @@
+
+
+-- 1 created by Callum
+Config.DoorList['AirportGates2-1'] = {
+    locked = true,
+    doorRate = 2.0,
+    objCoords = vec3(-977.517395, -2837.264404, 12.954859),
+    objYaw = 330.0,
+    distance = 5,
+    doorType = 'sliding',
+    authorizedJobs = { ['pilot'] = 0 },
+    doorLabel = 'Airport Gate 2',
+    fixText = false,
+    objName = 725274945,
+}
